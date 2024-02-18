19d8bae5739777dc31619ce93fa6ccdf7753bf2d
sw <sd.ace9719@gmail.com>
2024-02-18 18:04:50 +0530

    fixed cronjob.sh

--- deploy-cronjob.sh
+++ deploy-cronjob.sh
@@ -4,11 +4,11 @@ alias k=$(which kubectl)
 
 echo -e "Creating pvc for backup\n"
 
-k create -f backup-pvc.yaml
+k create -f manifests/backup-pvc.yaml
 
 echo ""
 
 echo -e "Creating cronjob for backup\n"
 
-k create -f cron.yaml
+k create -f manifests/cron.yaml
 
